a34bb9bc61e72c6921e6f57a2aff7b8579fd87bf
Syed Hassan Rizwan <hassan.rizwan@paysyslabs.com>
2026-01-20 17:22:05 +0500

    fix: update npm authentication token in publish workflow

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
 
+// @ts-expect-error -- module is resolved at runtime, types not available
 import { handleTransaction } from './rule';
 export { handleTransaction };
